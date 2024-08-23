bddd3d6dbd76adf4b313bed203f7e5f581c26e53
jedson Ferreira <jedazevedo@gmail.com>
2024-08-23 18:37:47 -0300

    alteração de rotas e preparação de arquivo para subir para o github, alteração das rotas no path_routes

--- path_routes.py
+++ path_routes.py
@@ -1,8 +1,8 @@
 # caminhos de busca
 import json
-CONTENT_EMAIL = 'C:\\Users\\jed\\Desktop\\base\\content_email.html'
-PATH_PASS = 'C:\\Users\\jed\\Desktop\\base\\dados_de_acesso.json'
-BASE_EMAIL = 'C:\\Users\\jed\\Desktop\\base\\Ebase.csv'
+CONTENT_EMAIL = 'C:/content_email.html'
+PATH_PASS = 'C:/dados_de_acesso.json'
+BASE_EMAIL = 'C:/Ebase.csv'
 
 with open(PATH_PASS, 'r') as senha:
     access_credences = json.load(senha)
